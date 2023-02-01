e8c9d73db2506c16d061d52900803b12545119fc
gabrielgatbonton <gabriel.gatbonton3262@gmail.com>
2023-02-01 11:18:02 +0800

    *ngIf directives

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
@@ -9,6 +10,39 @@ export class RoomsComponent {
   hotelName: string = 'Kotlin Hotel';
   hideRooms: boolean = false;
   numberOfRooms: number = 10;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  }
+  roomList: RoomList[] = [{
+    roomNumber: 1,
+    roomType: 'Deluxe Room',
+    amenities: 'Air Conditioner, Free WI-FI, TV, Bathroom, Kitchen',
+    price: 500,
+    photos: 'https://unsplash.com/photos/T5pL6ciEn-I',
+    checkInTime: new Date('11-Nov-2021'),
+    checkOutTime: new Date('12-Nov-2021'),
+  },
+  {
+    roomNumber: 2,
+    roomType: 'Deluxe Room',
+    amenities: 'Air Conditioner, Free WI-FI, TV, Bathroom',
+    price: 400,
+    photos: 'https://unsplash.com/photos/T5pL6ciEn-I',
+    checkInTime: new Date('12-Nov-2021'),
+    checkOutTime: new Date('13-Nov-2021'),
+  },
+  {
+    roomNumber: 3,
+    roomType: 'Private Suite',
+    amenities: 'Air Conditioner, Free WI-FI, TV, Bathroom, Bathtub',
+    price: 12000,
+    photos: 'https://unsplash.com/photos/T5pL6ciEn-I',
+    checkInTime: new Date('13-Nov-2021'),
+    checkOutTime: new Date('14-Nov-2021'),
+  }]
 
   toggle() {
     this.hideRooms = !this.hideRooms;
