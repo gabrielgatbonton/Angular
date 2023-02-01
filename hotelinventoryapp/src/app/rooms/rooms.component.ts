7f69c5f543c6c12a3d29c6fcde6206945fb2b4e4
gabrielgatbonton <gabriel.gatbonton3262@gmail.com>
2023-02-01 13:03:42 +0800

    Pipes

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -24,6 +24,7 @@ export class RoomsComponent {
     photos: 'https://unsplash.com/photos/T5pL6ciEn-I',
     checkInTime: new Date('11-Nov-2021'),
     checkOutTime: new Date('12-Nov-2021'),
+    rating: 9.45,
   },
   {
     roomNumber: 2,
@@ -33,6 +34,7 @@ export class RoomsComponent {
     photos: 'https://unsplash.com/photos/T5pL6ciEn-I',
     checkInTime: new Date('12-Nov-2021'),
     checkOutTime: new Date('13-Nov-2021'),
+    rating: 9.87,
   },
   {
     roomNumber: 3,
@@ -42,6 +44,7 @@ export class RoomsComponent {
     photos: 'https://unsplash.com/photos/T5pL6ciEn-I',
     checkInTime: new Date('13-Nov-2021'),
     checkOutTime: new Date('14-Nov-2021'),
+    rating: 9.95,
   }]
 
   toggle() {
